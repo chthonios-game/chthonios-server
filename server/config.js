450aa8917d96b961efb819bae31cd3529c369f4f
Lochie <afterlifelochie@afterlifelochie.net>
2016-10-14 18:45:29 +1100

    Add virtual server configuration structure.

--- server/config.js
+++ server/config.js
@@ -14,4 +14,25 @@ config.debug.classes.structure = process.env.DEBUG_CLASS_STR || false;
 config.debug.classes.cfr = process.env.DEBUG_CLASS_CFR || false;
 config.debug.classes.call = process.env.DEBUG_CLASS_EXEC || false;
 
+/** Virtual servers */
+config.servers = [];
+
+/**
+ * Default server.
+ * 
+ * <pre>
+ * name: server name
+ * sockets: list of ports to use on server; game: game port; map: map server port
+ * world: world file id
+ * </pre>
+ */
+config.servers.push({
+	name : "Test Realm",
+	sockets : {
+		game : 1357,
+		map : 9001
+	},
+	world : "0000-0000-0000-0001"
+});
+
 module.exports = config;
