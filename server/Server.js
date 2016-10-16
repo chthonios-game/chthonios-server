b48a187f46a943de4b2252e071e36befd0c2e3a4
Lochie <afterlifelochie@afterlifelochie.net>
2016-10-16 14:14:04 +1100

    add server load wrapper

--- server/Server.js
+++ server/Server.js
@@ -2,9 +2,18 @@ var Common = require("chthonios/rt/Common");
 
 var config = require("./config.js");
 var prefs = require("./config.prefs.js");
-var Env = Common.buildEnv(config, prefs);
 
-var GameServer = require('chthonios/game/GameServer');
+try {
+	var Env = Common.buildEnv(config, prefs);
 
-if (GameServer.Server.boot())
-	setInterval(Common.decoratedCallback(GameServer.Server.run, GameServer.Server), 100);
+	var GameServer = require('chthonios/game/GameServer');
+
+	if (GameServer.Server.boot()) {
+		console.info("Server ready:", "connect with ports `g=" + config.servers[0].sockets.game + "&m=" + config.servers[0].sockets.map
+				+ "` !");
+		setInterval(Common.decoratedCallback(GameServer.Server.run, GameServer.Server), 100);
+	}
+} catch (e) {
+	console.error("Unhandled server exception detected:");
+	console.error(e);
+}
